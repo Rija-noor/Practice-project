285f46ce08906230ee2a1386d0bcaf27b90b70ca
Naheel Huseyn <naheelkhan169@gmail.com>
2025-05-06 01:27:09 +0500

    i created separate brand pages and a js and card images folder in assets.

---
+++ assets/JS/haier.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $.getJSON("assets/JS/haier.json", function(data){
+        var container = $("#card_container")
+        data.forEach(card => {
+            var cardData = `
+            <div class="col-md-4 mb-4 d-flex justify-content-center">
+            <div class="card h-100" style="width: 18rem;">
+            <img src="assets/card-images/${card.image}"
+            class="card-img-top" alt="...">
+             <div class="card-body">
+            <h5 class="card-title text-center">${card.title}</h5>
+            <p class="card-text text-center">Rs.${card.price}/.</p>
+            <div class="d-grid gap-2 col-6 mx-auto">
+            <a href="#" class="btn btn-secondary btn-center">Add to Cart</a>
+            </div>
+            </div>
+            </div>
+            `
+           
+            container.append(cardData)
+        });
+    })
+})
